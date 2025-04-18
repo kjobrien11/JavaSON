6c66fe9d0a8548795dd2e9a11f7486a5566c17b8
KJ O’Brien <kjobrien@KJs-MacBook-Air.local>
2025-04-18 15:11:58 -0400

    basic working for one dimensional json object

--- javason/src/JavaSON.java
+++ javason/src/JavaSON.java
@@ -20,16 +20,36 @@ public class JavaSON {
     }
 
     public void parseJson(){
+        String key = "";
         for (int i = 0; i < rawJson.length(); i++) {
-            if(rawJson.charAt(i) == '{'){
+
+            if(rawJson.charAt(i) == ' '){
                 continue;
-            }else if (rawJson.charAt(i) == '"'){
-                char quote = '"';
-                int nextIndex = rawJson.indexOf(quote, i + 1);
-                String key = rawJson.substring(i + 1, nextIndex);
-                System.out.println(key);
-                i = nextIndex;
+            }
+//            else if (rawJson.charAt(i) == '"'){
+//                int nextQuoteIndex= rawJson.indexOf('"', i + 1);
+//                key = rawJson.substring(i + 1, nextQuoteIndex);
+//                System.out.println(key);
+//                i = nextQuoteIndex;
+//            }
+            else if (Character.isLetterOrDigit(rawJson.charAt(i))) {
+                int index = i;
+                StringBuilder value = new StringBuilder();
+                while (Character.isLetterOrDigit(rawJson.charAt(index))) {
+                    value.append(rawJson.charAt(index));
+                    index++;
+                }
+                System.out.println(value.toString());
+                i = index;
+
+                if (key.isEmpty()) {
+                    key = value.toString();
+                } else {
+                    json.put(key, value.toString());
+                    key = "";
+                }
             }
         }
+        System.out.println(json);
     }
 }
